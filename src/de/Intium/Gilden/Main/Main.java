f4c59000724430763b3d81856dbcb05a8e0c8692
modsytrypecoding <84596022+modsytrypecoding@users.noreply.github.com>
2021-06-22 17:13:12 +0200

    Update Main.java

--- src/de/Intium/Gilden/Main/Main.java
+++ src/de/Intium/Gilden/Main/Main.java
@@ -1,5 +1,23 @@
 package de.Intium.Gilden.Main;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	private static Main plugin;
+	
+	
+	public void onEnable() {
+		plugin = this;
+		
+		
+		PluginManager pl = Bukkit.getPluginManager();
+	}
+	
+	public static Main getPlugin() {
+		return plugin;
+	}
 
 }
+
