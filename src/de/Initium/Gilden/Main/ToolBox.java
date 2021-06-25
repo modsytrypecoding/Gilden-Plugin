3969ba1691009cb078127acf2b52c12692e43d54
Konrad <konrad.warttmann2@gmail.com>
2021-06-25 14:03:40 +0200

    Fix getallPlayers()

--- src/de/Initium/Gilden/Main/ToolBox.java
+++ src/de/Initium/Gilden/Main/ToolBox.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ToolBox extends JavaPlugin
@@ -86,8 +87,16 @@ public class ToolBox extends JavaPlugin
             if(key.contains("players"))
             {
                 //gilden.test_gilde.players
-            	p.sendMessage("Key: " + key + ":" + allUUIDs.toString());
-                allUUIDs.addAll(Main.getSaves().get("gilden." + key));
+            	Object test = Main.getSaves().get("gilden." + key);
+            	if(test instanceof String)
+                {
+                    allUUIDs.add((String) test);
+                }
+            	else if(test instanceof Collection<?>)
+                {
+                    allUUIDs.addAll((Collection<String>) test);
+                }
+                p.sendMessage("Key \"" + key + "\":" + test.toString());
             }
         }
         return allUUIDs;
